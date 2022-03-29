f54cb125b0d5bfa2a6dd50299fc865b0e3ce38fb
Miguel PALENCIA-OLIVAR <82161546+mpalenciaolivar@users.noreply.github.com>
2022-03-29 08:59:47 +0200

    Update requirements.R

--- requirements.R
+++ requirements.R
@@ -1,12 +1,26 @@
 pkgLoad <- function( packages = "favourites" ) {
   
   if( length( packages ) == 1L && packages == "favourites" ) {
-    # Inscrire les packages que vous souhaitez inclure ci-dessous, en respectant
-    # le formalisme suivant: c("package1", "package2")
-    packages <- c( "readxl" )
+    # Inscrire les packages que vous souhaitez inclure ci-dessous, en respectant le formalisme ci-dessous.
+    # De même, triez la liste des packages par ordre alphabétique.
+    # N.B: Les packages ci-dessous sont indiqués à titre d'illustration, ils ne sont pas forcément utiles et ne sont pas
+    # obligatoires pour le projet. Si vous avez reconnu les packages du TD 3, vous avez raison.
+    packages <- c(
+      "Amelia",
+      "aod",
+      "dplyr",
+      "e1071",
+      "GGally",
+      "ggplot2",
+      "MLmetrics",
+      "psych",
+      "ROSE",
+      "rpart",
+      "rpart.plot"
+    )
   }
   
-  packagecheck <- match( packages, utils::installed.packages()[,1] )
+  packagecheck <- match( packages, utils::installed.packages()[, 1] )
   
   packagestoinstall <- packages[ is.na( packagecheck ) ]
   
